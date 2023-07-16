a7bf4d90192af2e3b24af59387b512a415dc821f
stephane2000 <137523824+stephane2000@users.noreply.github.com>
2023-07-16 17:00:20 +0200

    Update urls.py

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -1,9 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('accueil/', views.accueil),
-    path('formulaire/', views.formulaire),
-    path('wordgenerator/', views.wordgenerator),
-]
+    path('', views.accueil, name='accueil'),
+    path('formulaire/', views.formulaire, name='formulaire'),
+    path('wordgenerator/', views.wordgenerator, name='wordgenerator'),
